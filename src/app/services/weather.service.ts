78aeb339ca9f8f14b9db5d51d47e1e79ccb82f71
viruop <virajrai706@gmail.com>
2023-04-25 10:42:19 +0530

    fixed env issue

--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -2,27 +2,23 @@ import { ObserversModule } from '@angular/cdk/observers';
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
+import { WeatherData } from '../models/weather.model';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class WeatherService {
-
-  constructor(private http: HttpClient) { 
-   
-    }
-    getWeatherData(cityName: string):Observable<WeatherData>{
-      this.http.get<WeatherData>(environment.WeatherApiBaseBaseUrl,{
-        headers:new HttpHeaders()
-        .set(environment.XRapidAPIHostHeaderName,environment.
-          XRapidAPIHost)
-        .set(environment.XRapidAPIKeyHeaderName,environment.XRapidAPIKey),
-        params:new HttpParams()
-        .set('q',cityName)
-        .set('units','metric')
-        .set('mode','json')
-      })
-
-    }
+  constructor(private http: HttpClient) {}
+  getWeatherData(cityName: string): Observable<WeatherData> {
+    return this.http.get<WeatherData>(environment.WeatherApiBaseBaseUrl, {
+      headers: new HttpHeaders()
+        .set(environment.XRapidAPIHostHeaderName, environment.XRapidAPIHost)
+        .set(environment.XRapidAPIKeyHeaderName, environment.XRapidAPIKey),
+      params: new HttpParams()
+        .set('q', cityName)
+        .set('units', 'metric')
+        .set('mode', 'json'),
+    });
+  }
 }
-
--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,1 +1,8 @@
-export const environment = {};
+export const environment = {
+  production: false,
+  WeatherApiBaseBaseUrl: 'https://openweather43.p.rapidapi.com/weather',
+  XRapidAPIKeyHeaderName: 'X-RapidAPI-Key',
+  XRapidAPIKey: 'cf68c3b727msh15c302946110a8ep13db83jsn7fc6064fe57a',
+  XRapidAPIHostHeaderName: 'X-RapidAPI-Host',
+  XRapidAPIHost: 'openweather43.p.rapidapi.com',
+};
